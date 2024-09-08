cc14624ba152fd2a18b741e640641b8e949a768e
Ioakeim Mingo <canonshai@gmail.com>
2024-09-08 18:25:43 +0100

    feat: Add draw_line method to Window class

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from window import Window
+from graphics import Window, Point, Line
 
 
 def main():
     w = Window(1920, 1080)
+    line = Line(Point(50, 50), Point(400, 400))
+    w.draw_line(line, "black")
     w.wait_for_close()
 
 
