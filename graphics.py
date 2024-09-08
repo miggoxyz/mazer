bd78182b8fa1ed535482bec79ae5ee824602ea5a
Ioakeim Mingo <canonshai@gmail.com>
2024-09-08 20:08:03 +0100

    Refactor main, implement cell, add unit tests for maze and maze skeleton.

--- graphics.py
+++ graphics.py
@@ -5,11 +5,11 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white",
                                height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -19,12 +19,13 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
-
-    def draw_line(self, line, fill_colour="black"):
-        line.draw(self.__canvas, fill_colour)
 
 
 class Point:
@@ -34,7 +35,11 @@ class Point:
 
 
 class Line:
-    def __init__(self, p1, p2):
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
         self.p1 = p1
         self.p2 = p2
 
---
+++ maze.py
@@ -0,0 +1,12 @@
+class Maze:
+    def __init__(self):
+        pass
+
+    def _create_cells(self):
+        pass
+
+    def _draw_cell(self, i, j):
+        pass
+
+    def _animate(self):
+        pass
